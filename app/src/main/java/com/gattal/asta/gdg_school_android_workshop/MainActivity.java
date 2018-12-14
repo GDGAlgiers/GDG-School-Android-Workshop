c99fd2c306e956d06c85cf162de442d940946dfd
Raouf <fa_gattal@esi.dz>
2018-12-14 17:51:54 +0100

    initial commit

---
+++ app/src/main/java/com/gattal/asta/gdg_school_android_workshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gattal.asta.gdg_school_android_workshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
